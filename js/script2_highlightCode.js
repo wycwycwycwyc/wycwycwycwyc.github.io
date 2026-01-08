8627bdfec73f61a50c797eda231ee36fb26a3503
wyc <3803002032@qq.com>
2026-01-08 21:09:22 +0800

    代码高亮修复

--- js/script2_highlightCode.js
+++ js/script2_highlightCode.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    hljs.highlightAll();
+    hljs.configure({
+    // 定义可能用到的语言
+    languages: ['javascript', 'python', 'html', 'bash', 'cpp', 'java', 'json'],
+    
+    // 高亮所有代码块
+}).highlightAll();
 });
